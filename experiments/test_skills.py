607d199e3e072a2b36971ba42a29bfd963723809
Anatoly Belikov <awbelikov@gmail.com>
2021-05-14 21:49:52 +0300

    training with turn action, log results

--- experiments/test_skills.py
+++ experiments/test_skills.py
@@ -1,4 +1,6 @@
 from operator import itemgetter
+from log import setup_logger
+import logging
 import numpy
 import network
 import torch
@@ -13,8 +15,8 @@ import time
 import numpy as np
 from tagilmo.utils.malmo_wrapper import MalmoConnector
 import tagilmo.utils.mission_builder as mb
-from common import * 
- 
+from common import *
+
 spiral = """
    <DrawLine x1="3"  y1="45" z1="1"  x2="8" y2="45" z2="2" type="sandstone" />
    <DrawLine x1="8"  y1="45" z1="2"  x2="10" y2="45" z2="4" type="sandstone" />         <!-- floor of the arena -->
@@ -26,7 +28,7 @@ spiral = """
    <DrawLine x1="6"  y1="45" z1="14"  x2="5" y2="45" z2="15" type="sandstone" />
    <DrawLine x1="5"  y1="45" z1="15"  x2="3" y2="45" z2="13" type="sandstone" />
 """
-  
+
 dec_xml = """
 <DrawingDecorator>
         <!-- coordinates for cuboid are inclusive -->
@@ -41,7 +43,7 @@ dec_xml = """
         <DrawItem    x="4"   y="46"  z="12" type="diamond" />
         <DrawItem    x="0"   y="46"  z="12" type="wooden_sword" />
 </DrawingDecorator>
-"""  
+"""
 
 modify_blocks = """
         <DrawLine x1="{0}"  y1="45" z1="5"  x2="4" y2="45" z2="0" type="sandstone"/>
@@ -77,10 +79,10 @@ my_mission.setViewpoint(1)
 
 
 max_retries = 3
-my_mission_record = mc.mission_record 
-mc.safeStart()
+my_mission_record = mc.mission_record
+#mc.safeStart()
 
-agent_host = mc.agent_hosts[0]  
+agent_host = mc.agent_hosts[0]
 
 
 class DeadException(RuntimeError):
@@ -91,7 +93,7 @@ class DeadException(RuntimeError):
 def collect_state(mc, target_pos):
     mc.observeProc()
     aPos = mc.getAgentPos()
-    print(aPos)
+    logging.debug(aPos)
     while aPos is None:
         sleep(0.1)
         mc.observeProc()
@@ -112,7 +114,7 @@ def collect_state(mc, target_pos):
     self_pitch = normAngle(aPos[3]*math.pi/180.)
     self_yaw = normAngle(aPos[4]*math.pi/180.)
     xpos, ypos, zpos = [_ % 1 for _ in aPos[0:3]]
-    print("%.2f" % xpos, "%.2f" % ypos, "%.2f" % zpos)
+    logging.debug("%.2f %.2f %.2f ", xpos, ypos, zpos)
     self_pos_enc = torch.as_tensor([self_pitch, self_yaw, xpos, ypos, zpos])
     return grid_enc, target_enc, self_pos_enc
 
@@ -150,18 +152,18 @@ def learn(agent, optimizer):
             optimizer.step()
             losses.append(loss.cpu().detach())
     if losses:
-        print('optimizing')
-        print('loss ', numpy.mean(losses))
-  
-    return losses
+        logging.debug('optimizing')
+        logging.debug('loss %f', numpy.mean(losses))
+
+    return numpy.mean(losses)
 
 
 def run_episode(agent, agent_host, eps, mc, optimizer):
-    print('memory: ', agent.memory.position)
+    logging.debug('memory: %i', agent.memory.position)
     agent.train()
 
     time.sleep(0.05)
-    max_t = 45 
+    max_t = 80
     eps_start = eps
     eps_end = 0.05
     eps_decay = 0.99
@@ -185,7 +187,7 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
 
     # obs = json.loads(world_state.observations[-1].text)
     # if 'XPos' not in obs:
-    #     print("skipping")
+    #     logging.debug("skipping")
     #     return 0, 0
     # curr_x = obs[u'XPos']
     # curr_z = obs[u'ZPos']
@@ -197,17 +199,18 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
     # pitch, yaw, xpos, ypos, zpos
     prev_pos = None
     prev_target_dist = None
-    prev_life = 20 
+    prev_life = 20
+    solved = False
 
     target =  ['lapis_block', 4.5, 46, 12.5]
     running = True
-    for i in range(5):
-        learn(agent, optimizer)
-    while running and t < max_t:
+    mean_loss = numpy.mean([learn(agent, optimizer) for _ in range(5)])
+    logging.info('loss %f', mean_loss)
+    while running:
         t += 1
         # target = search4blocks(mc, ['lapis_block'], run=False)
         reward = 0
-        print('\n\n')
+        logging.debug('\n\n')
         pos = target[1:4]
         try:
             grid_enc, target_enc, new_pos = collect_state(mc, pos)
@@ -222,7 +225,7 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
         else:
             # use only dist change for now
             life = mc.getLife()
-            print('current life ', life)
+            logging.debug('current life %f', life)
             if life == 0:
                 reward = -100
                 stop_motion(mc)
@@ -240,23 +243,24 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
                 agent.push_final(reward)
                 running = False
                 mc.sendCommand("quit")
+                solved = True
                 break
             world_state = agent_host.getWorldState()
             if not world_state.is_mission_running:
                 running = False
                 reward = -100
                 agent.push_final(reward)
-                break 
+                break
             if reward == 0:
                 reward -= 2.5
-            print("current reward ", reward)
+            logging.debug("current reward %f", reward)
             if not world_state.is_mission_running:
                 break
         data = dict(grid_vec=grid_enc, target=target_enc,
                 pos=new_pos)
         new_actions = agent(data, reward=reward, epsilon=eps)
         eps = max(eps * eps_decay, eps_end)
-        print('epsilon ', eps)
+        logging.debug('epsilon %f', eps)
         act(new_actions, mc)
         time.sleep(0.4)
         stop_motion(mc)
@@ -264,21 +268,23 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
         prev_pos = new_pos
         prev_target_dist = target_enc
         world_state = agent_host.getWorldState()
-    if t == max_t:
-        print("too long")
-        stop_motion(mc)
-        agent.push_final(-10)
-        mc.sendCommand("quit")
-        learn(agent, optimizer)
-    print("Final reward: %d" % reward)
-    total_reward += reward 
+        if t == max_t:
+            logging.debug("too long")
+            stop_motion(mc)
+            reward = -10
+            agent.push_final(-10)
+            mc.sendCommand("quit")
+            learn(agent, optimizer)
+            break
+        total_reward += reward
+    logging.debug("Final reward: %d" % reward)
 
-    return total_reward
+    return total_reward, t, solved
 
 
 def simple_trainable_agent_test_remastered():
-    # possible actions are 
-    # move[-1, 1], 
+    # possible actions are
+    # move[-1, 1],
     # strafe[-1, 1]
     # pitch[-1, 1]
     # turn[-1, 1]
@@ -287,14 +293,14 @@ def simple_trainable_agent_test_remastered():
                  network.ContiniousAction('strafe', -1, 1),
                  network.ContiniousAction('pitch', -1, 1),
                  network.ContiniousAction('turn', -1, 1),
-                 network.BinaryAction('jump')] 
+                 network.BinaryAction('jump')]
 
     # discreet actions
     # "move -0.5" "jump_forward",
-    action_names = ["strafe 0.35", "strafe -0.35", "move 0.5", "jump_forward" ]
+    action_names = ["turn 0.15", "turn -0.15", "move 0.5", "jump_forward" ]
     actionSet = [network.CategoricalAction(action_names)]
 
-    my_simple_agent = network.DQN(0.9, 70, 450, actionSet, capacity=1000,
+    my_simple_agent = network.DQN(0.9, 70, 450, actionSet, capacity=2000,
                                                     grid_len=27, grid_w=5,
                                                     target_enc_len=3,
                                                     pos_enc_len=5)
@@ -304,27 +310,27 @@ def simple_trainable_agent_test_remastered():
         #data.pop(name)
         #data.pop('action_output.6.bias')
         my_simple_agent.load_state_dict(data, strict=False)
-    num_repeats = 54000
-    cumulative_rewards = []
+    num_repeats = 400
+    comulative_rewards = []
     eps = 0.26
     eps_start = eps
     eps_end = 0.09
     eps_decay = 0.99
     optimizer = torch.optim.AdamW(my_simple_agent.parameters(), lr=0.0005,
                                     weight_decay=0.01)
-    p = 1 
+    p = 1
     for i in range(0, num_repeats):
         sp = ''
         # train on simple environment first
         if i < 200:
             p = random.choice([x for x in range(2, 8)])
         else:
-            p = random.choice([-3, -2, -1] + [x for x in range(0, 13)])
+            p = random.choice([-2, -1] + [x for x in range(0, 12)])
             if random.choice([True, False]):
                 sp = spiral
-        jump_block =  -3 
-        jump_block1 = random.choice(list(range(1, 11)))
-        print(jump_block, jump_block1)
+        jump_block =  -3
+        jump_block1 = random.choice(list(range(2, 11)))
+        logging.debug('%i, %i', jump_block, jump_block1)
         current_xml = dec_xml.format(modify_blocks.format(p, jump_block, jump_block1), sp)
         handlers = mb.ServerHandlers(mb.flatworld("3;7,220*1,5*3,2;3;,biome_1"), alldecorators_xml=current_xml, bQuitAnyAgent=True)
         agent_handlers = mb.AgentHandlers(observations=obs, all_str=mission_ending)
@@ -336,16 +342,14 @@ def simple_trainable_agent_test_remastered():
         my_simple_agent.clear_state()
 
 
-        print("\nMission %d of %d:" % (i + 1, num_repeats))
+        logging.debug("\nMission %d of %d:" % (i + 1, num_repeats))
         mc.safeStart()
-        print()
 
         # -- run the agent in the world -- #
-        cumulative_reward = run_episode(my_simple_agent, agent_host, eps, mc, optimizer)
-        print('Cumulative reward:')
-        print(cumulative_reward)
-        print("eps: %f" % eps)
-        cumulative_rewards += [cumulative_reward]
+        comulative_reward, steps, solved = run_episode(my_simple_agent, agent_host, eps, mc, optimizer)
+        logging.info('episode %i: solved %i: comulative reward: %f', i, solved, comulative_reward)
+        logging.debug("eps: %f", eps)
+        comulative_rewards += [comulative_reward]
         eps = max(eps * eps_decay, eps_end)
 
         # -- clean up -- #
@@ -354,4 +358,5 @@ def simple_trainable_agent_test_remastered():
         if i % 14 == 0:
             torch.save(my_simple_agent.state_dict(), 'agent.pth')
 
+setup_logger('train.log')
 simple_trainable_agent_test_remastered()

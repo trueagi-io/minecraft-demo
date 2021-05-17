7576c529d833792c5af04c83499e1a82398e0979
Anatoly Belikov <abelikov@singularitynet.io>
2021-05-17 21:27:35 +0400

    cleanup

--- experiments/test_skills.py
+++ experiments/test_skills.py
@@ -59,32 +59,6 @@ mission_ending = """
 </RewardForMissionEnd>
 """
 
-obs = mb.Observations()
-obs.gridNear = [[-1, 1], [-2, 2], [-1, 1]]
-
-current_xml = dec_xml.format(modify_blocks.format(4, -3, 8), '')
-handlers = mb.ServerHandlers(mb.flatworld("3;7,220*1,5*3,2;3;,biome_1"), alldecorators_xml=current_xml, bQuitAnyAgent=True)
-agent_handlers = mb.AgentHandlers(observations=obs, all_str=mission_ending)
-
-miss = mb.MissionXML(serverSection=mb.ServerSection(handlers), agentSections=[mb.AgentSection(name='Cristina',
-                     agenthandlers=agent_handlers,
-                     agentstart=mb.AgentStart([4.5, 46.0, 1.5, 30]))])
-
-
-mc = MalmoConnector(miss)
-my_mission = mc.mission
-my_mission.allowAllDiscreteMovementCommands()
-#my_mission.requestVideo(320, 240)
-my_mission.setViewpoint(1)
-
-
-max_retries = 3
-my_mission_record = mc.mission_record
-#mc.safeStart()
-
-agent_host = mc.agent_hosts[0]
-
-
 class DeadException(RuntimeError):
     def __init__(self):
         super().__init__("it's dead")
@@ -132,6 +106,39 @@ def act(actions, mc):
             mc.sendCommand(str(act))
 
 
+def init_mission(i, mc):
+    sp = ''
+    # train on simple environment first
+    if i < 10:
+        p = random.choice([x for x in range(2, 8)])
+    else:
+        p = random.choice([-2, -1] + [x for x in range(0, 12)])
+        if random.choice([True, False]):
+            sp = spiral
+    jump_block =  -3
+    jump_block1 = random.choice(list(range(2, 11)))
+    logging.debug('%i, %i', jump_block, jump_block1)
+    current_xml = dec_xml.format(modify_blocks.format(p, jump_block, jump_block1), sp)
+    handlers = mb.ServerHandlers(mb.flatworld("3;7,220*1,5*3,2;3;,biome_1"), alldecorators_xml=current_xml, bQuitAnyAgent=True)
+    video_producer = mb.VideoProducer(width=320, height=240)
+
+    obs = mb.Observations()
+    obs.gridNear = [[-1, 1], [-2, 2], [-1, 1]]
+
+
+    agent_handlers = mb.AgentHandlers(observations=obs,
+        all_str=mission_ending, video_producer=video_producer)
+
+    miss = mb.MissionXML(serverSection=mb.ServerSection(handlers), agentSections=[mb.AgentSection(name='Cristina',
+             agenthandlers=agent_handlers,
+             agentstart=mb.AgentStart([4.5, 46.0, 1.5, 30]))])
+    if mc is None:
+        mc = MalmoConnector(miss)
+    else:
+        mc.setMissionXML(miss)
+    return mc 
+
+
 def stop_motion(mc):
     mc.sendCommand('move 0')
     mc.sendCommand('strafe 0')
@@ -158,7 +165,10 @@ def learn(agent, optimizer):
     return numpy.mean(losses)
 
 
-def run_episode(agent, agent_host, eps, mc, optimizer):
+def run_episode(agent, eps, mc, optimizer):
+    """ Deep Q-Learning episode
+    """
+
     logging.debug('memory: %i', agent.memory.position)
     agent.train()
 
@@ -168,34 +178,13 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
     eps_end = 0.05
     eps_decay = 0.99
 
-    # Deep Q-Learning
-    #
-    # Params
-    # ======
-    #     n_episodes (int): maximum number of training epsiodes
-    #     max_t (int): maximum number of timesteps per episode
-    #     eps_start (float): starting value of epsilon, for epsilon-greedy action selection
-    #     eps_end (float): minimum value of epsilon
-    #     eps_decay (float): mutiplicative factor (per episode) for decreasing epsilon
-
-    scores = []  # list containing score from each episode
-    # scores_window = deque(maxlen=100)  # last 100 scores
     eps = eps_start
 
     is_first_action = True
     total_reward = 0
 
-    # obs = json.loads(world_state.observations[-1].text)
-    # if 'XPos' not in obs:
-    #     logging.debug("skipping")
-    #     return 0, 0
-    # curr_x = obs[u'XPos']
-    # curr_z = obs[u'ZPos']
-    states = {}
-    score = 0
     t = 0
 
-    world_state = agent_host.getWorldState()
     # pitch, yaw, xpos, ypos, zpos
     prev_pos = None
     prev_target_dist = None
@@ -241,16 +230,13 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
                 mc.sendCommand("quit")
                 solved = True
                 break
-            world_state = agent_host.getWorldState()
-            if not world_state.is_mission_running:
+            if not mc.is_mission_running():
                 logging.debug('failed in %i steps', t)
                 reward = -100
                 agent.push_final(reward)
                 break
             if reward == 0:
                 reward -= 2
-            if not world_state.is_mission_running:
-                break
         logging.debug("current reward %f", reward)
         data = dict(grid_vec=grid_enc, target=target_enc,
                 pos=new_pos)
@@ -263,7 +249,6 @@ def run_episode(agent, agent_host, eps, mc, optimizer):
         time.sleep(0.1)
         prev_pos = new_pos
         prev_target_dist = target_enc
-        world_state = agent_host.getWorldState()
         if t == max_t:
             logging.debug("too long")
             stop_motion(mc)
@@ -309,34 +294,16 @@ def simple_trainable_agent_test_remastered():
         #data.pop('action_output.6.bias')
         my_simple_agent.load_state_dict(data, strict=False)
     num_repeats = 400
-    comulative_rewards = []
     eps = 0.26
     eps_start = eps
     eps_end = 0.09
     eps_decay = 0.99
     optimizer = torch.optim.AdamW(my_simple_agent.parameters(), lr=0.0005,
                                     weight_decay=0.01)
-    p = 1
+    mc = None
     for i in range(0, num_repeats):
-        sp = ''
-        # train on simple environment first
-        if i < 200:
-            p = random.choice([x for x in range(2, 8)])
-        else:
-            p = random.choice([-2, -1] + [x for x in range(0, 12)])
-            if random.choice([True, False]):
-                sp = spiral
-        jump_block =  -3
-        jump_block1 = random.choice(list(range(2, 11)))
-        logging.debug('%i, %i', jump_block, jump_block1)
-        current_xml = dec_xml.format(modify_blocks.format(p, jump_block, jump_block1), sp)
-        handlers = mb.ServerHandlers(mb.flatworld("3;7,220*1,5*3,2;3;,biome_1"), alldecorators_xml=current_xml, bQuitAnyAgent=True)
-        agent_handlers = mb.AgentHandlers(observations=obs, all_str=mission_ending)
+        mc = init_mission(i, mc)
 
-        miss = mb.MissionXML(serverSection=mb.ServerSection(handlers), agentSections=[mb.AgentSection(name='Cristina',
-                 agenthandlers=agent_handlers,
-                 agentstart=mb.AgentStart([4.5, 46.0, 1.5, 30]))])
-        mc.setMissionXML(miss)
         my_simple_agent.clear_state()
 
 
@@ -344,10 +311,9 @@ def simple_trainable_agent_test_remastered():
         mc.safeStart()
 
         # -- run the agent in the world -- #
-        comulative_reward, steps, solved = run_episode(my_simple_agent, agent_host, eps, mc, optimizer)
+        comulative_reward, steps, solved = run_episode(my_simple_agent, eps, mc, optimizer)
         logging.info('episode %i: solved %i: comulative reward: %f', i, solved, comulative_reward)
         logging.debug("eps: %f", eps)
-        comulative_rewards += [comulative_reward]
         eps = max(eps * eps_decay, eps_end)
 
         # -- clean up -- #

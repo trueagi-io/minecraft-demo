88ceee524597a2c2ac1c28b19d97cf3683440fcf
Anatoly Belikov <abelikov@singularitynet.io>
2021-11-03 18:38:51 +0400

    add noise during training

--- experiments/train_vision_v2.py
+++ experiments/train_vision_v2.py
@@ -1,6 +1,7 @@
 """
 Train neural network on pairs of rgb and segmented images
 """
+import random
 import torch
 import os.path
 import numpy
@@ -9,10 +10,41 @@ from tagilmo.utils import segment_mapping
 from dataset import MinecraftSegmentation
 
 
+def random_transformer(x, transformers=[]):
+    orig_shape = x.shape
+    assert (len(orig_shape) == 3)
+    assert (orig_shape[2] <= orig_shape[0])
+    assert (orig_shape[2] <= orig_shape[1])
+
+    x = x.transpose(2, 0, 1)
+    for transform in transformers:
+        assert 'resize' not in str(transform.__class__).lower()
+        if random.random() < 0.25:
+            x_std = x.std()
+            if x_std < 0.01:
+                continue
+            new_x = transform(x)
+            if new_x.std() < 0.1 * x_std:
+                print("{0} decreased std to {1} from {2}".format(transform, new_x.std(), x_std))
+                continue
+            x = new_x
+    x = x.astype(numpy.float32)
+    x = x.transpose(1, 2, 0)
+    return x
+
+
+class RandomTransformer:
+    def __init__(self, transformers):
+        self.transformers = transformers
+
+    def __call__(self, x):
+        return random_transformer(x, self.transformers)
+
+
 reverse_map = {v: k for k, v in segment_mapping.items()}
 
 # merge second to first
-to_merge = ('log/oak', 'log/birch'), ('leaves/oak', 'leaves/birch')
+to_merge = ('log/oak', 'log/birch'), ('leaves/oak', 'leaves/birch'), ('leaves/oak', 'vine'), ('log/oak', 'log/spruce')
 to_train = ['log/oak', 'leaves/oak', 'dirt', 'grass', 'lava', 'water', 'stone/stone']
 to_train = ['log/oak', 'leaves/oak']
 train_id = [reverse_map[k] for k in to_train]
@@ -41,10 +73,27 @@ def transform_item_1channel(item):
     return image, segm_image1
 
 
+def make_noisy_transformers():
+    from noise import AdditiveGaussian, RandomBrightness, AdditiveShade, MotionBlur, SaltPepper, RandomContrast
+    # ColorInversion doesn't seem to be usefull on most datasets
+    transformer = [
+                   AdditiveGaussian(var=30),
+                   RandomBrightness(range=(-50, 50)),
+                   AdditiveShade(kernel_size_range=[45, 85],
+                                 transparency_range=(-0.25, .45)),
+                   SaltPepper(),
+                   MotionBlur(max_kernel_size=5),
+                   RandomContrast([0.6, 1.05])
+                   ]
+    return RandomTransformer(transformer)
+
+random_t = make_noisy_transformers()
+
 def transform_item_nchannel(item):
     image, segm_image = item
 
     height, width, _ = image.shape
+    assert height == 240 * 4
     image = cv2.resize(image, (int(width * RESIZE), int(height * RESIZE)),
                 fx=RESIZE, fy=RESIZE, interpolation=cv2.INTER_NEAREST)
 
@@ -57,7 +106,10 @@ def transform_item_nchannel(item):
     for i, t in enumerate(to_train):
         mask[:, :, i + 1] = (segm_image1 == reverse_map[to_train[i]])
     mask[:, :, 0] = ~ (mask[:, :, 1:].sum(2) > 0)
-    img = image.transpose(2, 0, 1) / 255
+
+    image1 = random_t(image)
+    # cv2.imshow('transformed', image1.astype(numpy.uint8))
+    img = image1.transpose(2, 0, 1) / 255
     return img.astype(numpy.float32), mask.transpose(2, 0, 1)
 
 
@@ -70,7 +122,7 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     train_depth = False
     train = True
-    n_epochs = 22
+    n_epochs = 100
     batch_size = 22
 
     data_set = MinecraftSegmentation(imagedir='image_data1',
@@ -78,7 +130,7 @@ if __name__ == '__main__':
     data_set[44]
     loader = DataLoader(data_set, batch_size=batch_size, shuffle=True)
     # +1 for None
-    net = GoodPoint(8, len(to_train) + 1, n_channels=3, depth=train_depth).to(device)
+    net = GoodPoint(8, len(to_train) + 1, n_channels=3, depth=train_depth, batchnorm=False).to(device)
     model_path = 'goodpoint.pt'
     if os.path.exists(model_path):
         model_weights = torch.load(model_path, map_location=device)['model']
@@ -89,7 +141,7 @@ if __name__ == '__main__':
     epochs = 30
     eps = 0.0000001
     optimizer = optim.AdamW(net.parameters(), lr=lr)
-    show = True
+    show = False
     for epoch in range(n_epochs):
         for j, batch in enumerate(loader):
             optimizer.zero_grad()
@@ -106,10 +158,12 @@ if __name__ == '__main__':
             block_sum = block_count.sum()
             ratio = block_count / block_sum
             weights = torch.as_tensor([1 if x == 0 else 1 / x for x in ratio])
+            # weights = torch.as_tensor([0.1, 1, 1]).unsqueeze(0).unsqueeze(2).unsqueeze(3).to(logprob)
             # don't use weighting for now
             # logprob *= weights.unsqueeze(0).unsqueeze(2).unsqueeze(3).to(logprob)
-            loss = (- (logprob * target)).mean()
+            loss = 0
             if train:
+                loss = (- (logprob * target)).mean()
                 loss.backward()
                 optimizer.step()
             if j % 10 == 0:

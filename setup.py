a8c284ac787c7e8b324a7a3a675b15a96aad2662
Anatoly Belikov <abelikov@singularitynet.io>
2022-11-03 13:10:56 +0400

    add VereyaPython to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='tagilmo',
       version='0.0.1',
       packages=['tagilmo',
+                'tagilmo.VereyaPython',
                 'tagilmo.utils'],
       install_requires=[]#'MalmoPython']
 )

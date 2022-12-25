371c7cd419175c870668965acdcbb6c8d29acc52
Anatoly Belikov <abelikov@singularitynet.io>
2022-12-25 23:43:47 +0400

    add simple test runner

---
+++ experiments/vereya/run_tests.py
@@ -0,0 +1,27 @@
+import sys
+import unittest
+
+
+def run_tests(test_files):
+    # create a test suite from the list of test files
+    suite = unittest.TestSuite()
+    for test_file in test_files:
+        # load the tests from the file
+        tests = unittest.defaultTestLoader.loadTestsFromName(test_file)
+        # add the tests to the suite
+        suite.addTests(tests)
+    # create a test runner and run the tests
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
+    return result
+
+
+def main():
+    test_files = ['test_craft', 'test_inventory']
+    res = run_tests(test_files)
+    if not res.wasSuccessful():
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()

db05ff02d943f0190d64fbffe5d986c2de0147a9
DaddyWesker <avenger15@yandex.ru>
2023-01-19 09:55:19 +0300

    Update test_horizon.py
    Remove duplicate

--- experiments/test_horizon.py
+++ experiments/test_horizon.py
@@ -11,7 +11,6 @@ from tagilmo.utils.mission_builder import AgentStart
 from tagilmo.utils.malmo_wrapper import MalmoConnector, RobustObserverWithCallbacks
 from tagilmo.utils import segment_mapping
 from tagilmo.utils.mathutils import degree2rad
-import tagilmo.utils.mission_builder as mb
 from tagilmo.VereyaPython import setupLogger
 
 

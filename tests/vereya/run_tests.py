e5ae59cabb71b2cae90f70562d2018b0b66d8b72
Anatoly Belikov <abelikov@singularitynet.io>
2023-03-24 10:26:49 +0300

    add tests for motion

--- tests/vereya/run_tests.py
+++ tests/vereya/run_tests.py
@@ -26,7 +26,9 @@ def run_tests(test_files):
 
 def main():
     VereyaPython.setupLogger()
-    test_files = ['test_craft', 'test_inventory', 'test_quit', 'test_observation', 'test_placement' ]
+    test_files = ['test_motion_vereya', 'test_craft', 
+                  'test_inventory', 'test_quit', 
+                  'test_observation', 'test_placement' ]
     res = run_tests(test_files)
     if not res.wasSuccessful():
         sys.exit(1)
--- tests/vereya/test_motion_vereya.py
+++ tests/vereya/test_motion_vereya.py
@@ -1,105 +1,85 @@
 import logging
 import json
 import time
+import unittest
 import tagilmo.utils.mission_builder as mb
 from tagilmo.utils.vereya_wrapper import MCConnector, RobustObserver
+from base_test import BaseTest
+from tagilmo import VereyaPython
+from common import init_mission
 
 
-def init_mission(mc, start_x=None, start_y=None):
-    want_depth = False
-    video_producer = mb.VideoProducer(width=320 * 4,
-                                      height=240 * 4, want_depth=want_depth)
+logger = logging.getLogger(__file__)
 
-    obs = mb.Observations()
-    obs.gridNear = [[-1, 1], [-2, 1], [-1, 1]]
+class TestMotion(BaseTest):
 
+    mc = None
+    obs = None
 
-    agent_handlers = mb.AgentHandlers(observations=obs, video_producer=video_producer)
+    @classmethod
+    def setUpClass(cls, *args, **kwargs):
+        start = (-151.0, -213.0)
+        mc, obs = init_mission(None, start_x=start[0], start_y=start[1], forceReset='true', seed='43')
+        cls.mc = mc
+        cls.obs = obs
+        obs.update_in_background()
+        assert mc.safeStart()
+        time.sleep(4)
 
-    print('starting at ({0}, {1})'.format(start_x, start_y))
+    def test_motion_forward(self):
+        obs = self.obs
+        mc = self.mc
+        pos0 = obs.getCachedObserve('getAgentPos')
+        logger.info('position ' + str(pos0))
+        print('sending command move 1')
+        mc.sendCommand('move 1')
+        time.sleep(1)
 
-    #miss = mb.MissionXML(namespace="ProjectMalmo.microsoft.com",
-    miss = mb.MissionXML(
-                         agentSections=[mb.AgentSection(name='Cristina',
-             agenthandlers=agent_handlers,
-                                      #    depth
-             agentstart=mb.AgentStart([start_x, 74.0, start_y, 1]))])
-    flat_json = {"biome":"minecraft:plains",
-                 "layers":[{"block":"minecraft:diamond_block","height":1}],
-                 "structures":{"structures": {"village":{}}}}
+        pos_move1 = obs.getCachedObserve('getAgentPos')
+        logger.info('position ' + str(pos_move1))
+        self.assertNotEqual(pos0, pos_move1, 'move 1 sent but position is the same')
+        print('sending command move 0')
+        mc.sendCommand('move 0')
+        time.sleep(0.5)
 
-    flat_param = "3;7,25*1,3*3,2;1;stronghold,biome_1,village,decoration,dungeon,lake,mineshaft,lava_lake"
-    flat_json = json.dumps(flat_json).replace('"', "%ESC")
-    world = mb.defaultworld(
-        seed='5',
-        forceReuse="true",
-        forceReset="false")
-    flat_world = mb.flatworld(flat_json,
-                    seed='43',
-                    )
-    miss.setWorld(world)
-    miss.serverSection.initial_conditions.allowedmobs = "Pig Sheep Cow Chicken Ozelot Rabbit Villager"
-    # uncomment to disable passage of time:
-    miss.serverSection.initial_conditions.time_pass = 'false'
-    miss.serverSection.initial_conditions.time_start = "1000"
+        pos_move0 = obs.getCachedObserve('getAgentPos')
+        time.sleep(1)
+        pos1_move0 = obs.getCachedObserve('getAgentPos')
+        logger.info('position ' + str(pos1_move0))
+        time.sleep(1)
+        pos2_move0 = obs.getCachedObserve('getAgentPos')
+        logger.info('position ' + str(pos2_move0))
+        self.assertEqual(pos1_move0, pos2_move0)
 
-    if mc is None:
-        mc = MCConnector(miss)
-        obs = RobustObserver(mc)
-    else:
-        mc.setMissionXML(miss)
-    return mc, obs
+    def test_rotate(self):
+        obs = self.obs
+        mc = self.mc
 
+        PY = slice(3, None)
+        XYZ = slice(None, 3)
+        print('sending command turn 0.1')
+        pos0 = obs.getCachedObserve('getAgentPos')
+        mc.sendCommand('turn 0.1')
+        time.sleep(1)
+        pos1 = obs.getCachedObserve('getAgentPos')
 
-def test_basic_motion():
-    pass 
-
-
-def main():
-    from tagilmo import VereyaPython
-#    VereyaPython.setLoggingComponent(VereyaPython.LoggingComponent.LOG_TCP, True)
-#    VereyaPython.setLogging('log.txt', VereyaPython.LoggingSeverityLevel.LOG_FINE)
-
-    start = 316.5, 5375.5
-    start = (-108.0, -187.0)
-    mc, obs = init_mission(None, start_x=start[0], start_y=start[1]) 
-
-    mc.safeStart()
-    time.sleep(4)
-    print('sending command')
-    mc.sendCommand('move 1')
-        
-    #print('send chat')
-    #obs.sendCommand("chat /difficulty peaceful")
-    time.sleep(1)
-    print('sending command')
-    mc.sendCommand('move 1')
-        
-
-    time.sleep(1)
-    print('sending command move 0')
-    mc.sendCommand('move 0')
-    print('sending command turn 0.1')
-    mc.sendCommand('turn 0.1')
-    mc.observeProc()
-    pos = mc.getAgentPos()
-    print('sending command turn 0')
-    time.sleep(1)
-    mc.sendCommand('turn 0')
-    time.sleep(1)
-    mc.observeProc()
-    pos1 = mc.getAgentPos()
-    while True:
-       mc.observeProc()
-       time.sleep(2)
-       print('waiting')
-       img_frame = mc.frames[0] 
-       if img_frame is None:
-           print(img_frame)
-       ray = mc.getLineOfSights()
-       print(ray)
+        print('sending command turn 0')
+        mc.sendCommand('turn 0')
+        time.sleep(1)
+        pos2 = obs.getCachedObserve('getAgentPos')
+        self.assertNotEqual(pos2[PY], pos1[PY])
+        self.assertEqual(pos2[XYZ], pos0[XYZ])
 
 
-        
+    def teadDown(self):
+        self.obs.stopMove()
+        super().tearDown()
+
+    def setUp(self):
+        self.obs.stopMove()
+        super().setUp()
+
+
 if __name__ == '__main__':
-   main()
+    VereyaPython.setupLogger()
+    unittest.main()

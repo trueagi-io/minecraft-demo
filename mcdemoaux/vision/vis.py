1a0cdb006e4ac87666bf70475c91d0ceccf2086c
Innokenty <innokenty@singularitynet.io>
2023-05-02 12:24:40 +0300

    remove unnecessary color convertion

--- mcdemoaux/vision/vis.py
+++ mcdemoaux/vision/vis.py
@@ -22,10 +22,6 @@ class Visualizer(threading.Thread):
             while self.queue:
                 with self._lock:
                     data = self.queue.pop()
-                    if len(data[1].shape) == 3:
-                        image = cv2.cvtColor(data[1], cv2.COLOR_BGR2RGB)
-                    else:
-                        image = data[1]
-                cv2.imshow(data[0], image)
+                cv2.imshow(data[0], data[1])
             cv2.waitKey(300)
 

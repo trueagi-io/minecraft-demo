ce7e09abca638e01b9325fff87d94e9637c78f87
Innokenty <innokenty@singularitynet.io>
2023-05-26 18:08:38 +0300

    maximum size and some fixes

--- mcdemoaux/vision/neural.py
+++ mcdemoaux/vision/neural.py
@@ -8,50 +8,44 @@ import cv2
 model_cache = dict()
 
 
-def process_pixel_data(pixels, keep_aspect_ratio=False, maximum_area=None):
-    # img_data = numpy.frombuffer(pixels, dtype=numpy.uint8)
+def process_pixel_data(pixels, keep_aspect_ratio, maximum_size):
     height, width, _ = pixels.shape
-    wscale = width / 320
-    if keep_aspect_ratio:
-        hscale = height / 240
-    else:
-        hscale = wscale
-
+    aspect_ratio_org = width / height
+    wscale = width // 320
     scaled_width = int(320*wscale)
-    scaled_height = int(240*hscale)
+    scaled_height = int(int(keep_aspect_ratio) * (scaled_width / aspect_ratio_org) + (1 - int(keep_aspect_ratio)) * int(240*wscale))
+    if maximum_size is not None and maximum_size:
+        scaled_width = maximum_size[0] if scaled_width > maximum_size[0] else scaled_width
+        if not keep_aspect_ratio:
+            scaled_height = maximum_size[1] if scaled_height > maximum_size[1] else scaled_height
+        else:
+            scaled_height = int(scaled_width / aspect_ratio_org)
 
-    #to make width and height divisible by 8
+    # to make width and height divisible by 8
     scaled_width -= scaled_width % 8
     scaled_height -= scaled_height % 8
 
     img_data = cv2.resize(pixels, dsize=(scaled_width, scaled_height), interpolation=cv2.INTER_CUBIC)
-    # img_data = numpy.resize(pixels, (240 * scale, 320 * scale, 4))
-    # img_data = pixels
-    # if resize != 1:
-    #     height, width, _ = img_data.shape
-    #     img_data = cv2.resize(img_data, (int(width * resize), int(height * resize)),
-    #         fx=resize, fy=resize, interpolation=cv2.INTER_NEAREST)
 
     return img_data
 
 
-def get_image(img_frame, keep_aspect_ratio=False, maximum_area=None):
+def get_image(img_frame, keep_aspect_ratio, maximum_size):
     if img_frame is not None:
-        return process_pixel_data(img_frame.pixels, keep_aspect_ratio, maximum_area)
+        return process_pixel_data(img_frame.pixels, keep_aspect_ratio, maximum_size)
     return None
 
 
 class NeuralWrapper:
-    def __init__(self, rob, keep_aspect_ratio=False, maximum_area=None):
+    def __init__(self, rob, keep_aspect_ratio=False, maximum_size=(320, 240)):
         self.net = self.load_model()
         self.rob = rob
         self.keep_aspect_ratio = keep_aspect_ratio
-        self.maximum_area = maximum_area
+        self.maximum_size = maximum_size
 
     def _get_image(self):
         img_name = 'getImageFrame'
-        img_data = get_image(self.rob.getCachedObserve(img_name), self.keep_aspect_ratio, self.maximum_area)
-        # img_data = self.rob.getCachedObserve(img_name).pixels
+        img_data = get_image(self.rob.getCachedObserve(img_name), self.keep_aspect_ratio, self.maximum_size)
         if img_data is not None:
             img_data = torch.as_tensor(img_data).permute(2,0,1)
             img_data = img_data.unsqueeze(0) / 255.0

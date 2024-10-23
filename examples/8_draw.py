97923d747c870e52e047bdc00902b6c450fa47de
Roman Kozin <enatik14@gmail.com>
2024-10-23 14:33:08 +0300

    example to test draw functions

---
+++ examples/8_draw.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(1, '../minecraft-demo')
+from tagilmo.utils.vereya_wrapper import MCConnector
+import tagilmo.utils.mission_builder as mb
+
+draw_str = """<DrawCuboid x1="0" y1="-60" z1="10" x2="2" y2="-59" z2="13" type="stone"/>
+                <DrawBlock x="8" y="-60" z="10" type="cobblestone"/>
+                <DrawItem x="10" y="-60" z="10" type="diamond"/>
+                <DrawLine x1="10" y1="-60" z1="4" x2="14" y2="-60" z2="11" type="sandstone"/>"""
+                #TODO: implement drawSphere  <DrawSphere x="4" y="-55" z="12" radius="3" type="lapis_block"/>
+miss = mb.MissionXML(serverSection=mb.ServerSection(handlers=mb.ServerHandlers(drawingdecorator_xml=draw_str)))
+miss.setWorld(mb.flatworld("",
+                           seed= '5',
+              forceReset = "true"))
+miss.setObservations(mb.Observations())
+# we typically don't need this for real Minecraft agents, but this example just checks the connection to Minecraft
+miss.setTimeLimit(5000)
+
+mc = MCConnector(miss)
+# Note that we need two instances of Minecraft running
+mc.safeStart()
+
+mc.stop()

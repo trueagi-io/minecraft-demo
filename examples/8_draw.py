71e49c63642c7f6d6ad43e4996bae4de9d355b28
Roman Kozin <enatik14@gmail.com>
2024-10-25 13:29:01 +0300

    updated example for drawing objects

--- examples/8_draw.py
+++ examples/8_draw.py
@@ -1,12 +1,13 @@
 from tagilmo.utils.vereya_wrapper import MCConnector
 import tagilmo.utils.mission_builder as mb
 
-draw_str = """<DrawCuboid x1="0" y1="-60" z1="10" x2="2" y2="-59" z2="13" type="stone"/>
-                <DrawBlock x="8" y="-60" z="10" type="cobblestone"/>
-                <DrawItem x="10" y="-60" z="10" type="diamond"/>
-                <DrawLine x1="10" y1="-60" z1="4" x2="14" y2="-60" z2="11" type="sandstone"/>"""
-                #TODO: implement drawSphere  <DrawSphere x="4" y="-55" z="12" radius="3" type="lapis_block"/>
-miss = mb.MissionXML(serverSection=mb.ServerSection(handlers=mb.ServerHandlers(drawingdecorator_xml=draw_str)))
+block = mb.DrawBlock("-1", "-60", "4", "cobblestone")
+it = mb.DrawItem(0, -60, 0, "diamond")
+cuboid = mb.DrawCuboid("1", -60, "0", 3, -60, 3, "sandstone")
+line = mb.DrawLine(5, -60, 3, 9, -55, 8, "diamond_ore")
+draw = mb.DrawingDecorator([block, it, cuboid, line])
+
+miss = mb.MissionXML(serverSection=mb.ServerSection(handlers=mb.ServerHandlers(drawingdecorator=draw)))
 miss.setWorld(mb.flatworld("",
                            seed= '5',
               forceReset = "true"))

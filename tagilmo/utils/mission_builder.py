97adefea3cd5fe66f52fb25f88109febd2660930
Roman Kozin <enatik14@gmail.com>
2024-12-20 17:31:35 +0300

    fixed ModSettings

--- tagilmo/utils/mission_builder.py
+++ tagilmo/utils/mission_builder.py
@@ -24,7 +24,7 @@ class ModSettings:
             _xml += "<MsPerTick>"+self.ms_per_tick+"</MsPerTick>"
         else:
             _xml += "<MsPerTick/>"
-        _xml += '\n<ModSettings>\n'
+        _xml += '\n</ModSettings>\n'
         return _xml
 
 
@@ -608,7 +608,7 @@ class MissionXML:
 <Mission xmlns="http://{0}" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 '''.format(namespace)
         _xml += self.about.xml()
-        # _xml += self.modSettings.xml()
+        _xml += self.modSettings.xml()
         _xml += self.serverSection.xml()
         for agentSection in self.agentSections:
             _xml += agentSection.xml()
